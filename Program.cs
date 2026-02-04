2cb9a5de09cf84bf09b01d9e3cda2d86e4a1d176
Бугаёв Богдан Олегович <bugayovbogdan@yandex.ru>
2026-02-04 12:48:45 +0500

    добавлен класс калькулятора

--- Program.cs
+++ Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static class Calculator
         {
-            Console.WriteLine("Hello, World!");
+            // Сложение двух чисел
+            public static double Sum(double a, double b)
+            {
+                return a + b;
+            }
+            // Вычитание двух чисел
+            public static double Substract(double a, double b)
+            {
+                return a - b;
+            }
+            // Умножение двух чисел
+            public static double Multiply(double a, double b)
+            {
+                return a * b;
+            }
+            // Деление двух чисел
+            public static double Divide(double a, double b)
+            {
+                return a / b;
+            }
         }
     }
 }
